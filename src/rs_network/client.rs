d90237d3b6151ac2a032a3a3ee4f955f8eb5a32b
cowrat <cowrat@rat-trap.io>
2024-09-14 22:30:31 +0100

    initial client functionality

--- src/rs_network/client.rs
+++ src/rs_network/client.rs
@@ -1,25 +1,43 @@
-use std::io::{stdin, Write};
+use std::io::{prelude::*, stdin,stdout, Write, BufReader};
 use std::net::TcpStream;
+use std::thread;
 
 pub fn connect(){
     
     if let Ok(mut stream) = TcpStream::connect("127.0.0.1:33777"){
         // We have connected to the server
-
-        // let mut input_stream = stream.try_clone().unwrap();
-        // let output_stream = &mut stream;
-
-	    let mut user_buffer = String::new();
-
         println!("Connected to server!");
 
+        // User Buffer
+        let mut user_buffer = String::new();
+        let recvstream = stream.try_clone().unwrap();
+        thread::spawn(|| {
+            msg_recv(recvstream);
+        });
+        
+
         loop{
+            stdout().flush().unwrap(); // allows for the input to be on same line as previous print
             stdin().read_line(&mut user_buffer).unwrap();
-            stream.write(user_buffer.as_bytes()).unwrap();
+            stream.write_all(user_buffer.as_bytes()).unwrap();
+            user_buffer.clear();
             stream.flush().unwrap();
+
         }
 
     }else{
         println!("Couldn't connect to server...")
     }
+}
+
+
+pub fn msg_recv(stream: TcpStream){
+    let mut reader = BufReader::new(stream);
+    let mut server_buffer = String::new();
+    loop{
+        reader.read_line(&mut server_buffer).unwrap();
+        println!("RECV: {}", server_buffer.trim());
+        server_buffer.clear();
+    }
+    
 }
