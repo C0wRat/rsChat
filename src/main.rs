af6e26dd555331d9adc947c0caa60a590b7d1e58
cowrat <cowrat@rat-trap.io>
2024-09-21 22:10:04 +0100

    early build

--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,8 @@ fn menu(){
     let mut input: String = String::new();
     
     println!("1.) Change Username");
-    println!("2.) Host Chat Room");
-    println!("3.) Join Chat Room");
+    println!("2.) Join Chat Room");
+    println!("3.) Host Chat Room");
     println!("4.) Exit rsChat");
 
     // Get User Input.
@@ -42,8 +42,8 @@ fn menu(){
 
     match option{
         1 => println!("Feature Missing!"),
-        2 => rs_network::server::host_server(),
-        3 => rs_network::client::connect(),
+        2 => start_client(),
+        3 => rs_network::server::host_server(),
         4 => process::exit(0),
         _=> println!("Invalid Option!")
     }
@@ -56,4 +56,9 @@ fn main() {
         menu();
         clear();
     }
+}
+
+fn start_client(){
+    rs_network::server::start_web_server();
+    rs_network::client::connect();
 }
--- src/rs_network/server.rs
+++ src/rs_network/server.rs
@@ -9,15 +9,19 @@ use std::time::Duration;
 
 
 pub fn host_server(){
-    let _ = fs::File::create("msg.txt"); // Probably should make this safer!
+    // print!("\x1Bc");
+    // println!("[+] Hosting new server.");
+    // let _ = fs::File::create("msg.txt"); // Probably should make this safer!
+    let _ = fs::File::create("server_messages.txt");
     thread::spawn(|| {
         create_server();
     });
 }
 
 pub fn create_server(){
+    // println!("[+] Server Thread Started");
     let listener = TcpListener::bind("127.0.0.1:33777").unwrap();
-
+    println!("[+] Server Listening");
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         let send_stream = stream.try_clone().unwrap();
@@ -29,16 +33,12 @@ pub fn create_server(){
         thread::spawn(|| {
             sender(send_stream);
         });
-
-
-
-        
-    }    
+    }
+    // println!("[-] Server CRASH!!!");    
 
 }
 
 pub fn session(stream: TcpStream){
-    // println!("Connection established!");
         let mut reader = BufReader::new(stream);
         let mut server_buffer = String::new();
 
@@ -56,33 +56,97 @@ pub fn session(stream: TcpStream){
 
 pub fn log(username: &String, message: &String){
     let data = format!("{}: {}\n",username.trim(), message.trim());
-    // let _ = fs::write("msg.txt", data);  //probably should make this safer
-
-    let mut data_file = fs::OpenOptions::new()
+    // println!("Received {}", data);
+    // let mut data_file = fs::File::open("server_messages.txt").unwrap();
+     let mut data_file = fs::OpenOptions::new()
         .append(true)
-        .open("msg.txt")
+        .open("server_messages.txt")
         .expect("cannot open file");
+    // let _ = data_file.write("".as_bytes());
     let _ = data_file.write(data.as_bytes()); // Make this safer!
 }
 
 
 pub fn sender(mut stream: TcpStream){
-    let mut data_file = fs::File::open("msg.txt").unwrap();
+    let mut data_file = fs::File::open("server_messages.txt").unwrap();
 
     // Create an empty mutable string
     let mut file_content = String::new();
+    let mut content_buffer: String = String::new();
 
     loop{
-        thread::sleep(Duration::from_millis(100));
+        // thread::sleep(Duration::from_millis(1000));
         // Copy contents of file to a mutable string
         data_file.read_to_string(&mut file_content).unwrap();
+        if (file_content.trim() != "") & (file_content != content_buffer){
 
-        if file_content.trim() != ""{
-            // println!("{}", file_content.trim());
-
+            let x = file_content.to_string();
+            let d: Vec<_> = x.split('\n').collect();
+            let lines = d.len().to_string() + "\n";
+            
+            stream.write_all(lines.as_bytes()).unwrap();
+            // println!("------------ server-send-start ----------------");
+            // println!("sending : {}", file_content.trim());
+            // println!("------------------- end ------------------");
             stream.write_all(file_content.as_bytes()).unwrap();
             stream.flush().unwrap();
+            content_buffer = file_content.clone();
         }
         
     }
+}
+
+pub fn start_web_server(){
+    thread::spawn(|| {
+        web_server();
+    });
+
+}
+
+pub fn web_server(){
+
+    let listener = TcpListener::bind("127.0.0.1:9000").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_web_connection(stream);
+    }
+}
+
+fn handle_web_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+    .lines()
+    .map(|result| result.unwrap())
+    .take_while(|line| !line.is_empty())
+    .collect();
+
+    // println!("{http_request:#?}");
+    // println!("{}", http_request[0]);
+    let (status_line, filename) = if http_request[0] == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 200 OK", "recv.txt")
+    };
+
+    if http_request[0] == "POST /message HTTP/1.1"{
+        // println!("{}", http_request[2]);
+        let data = http_request[2].to_string() + "\n";
+        let msg:Vec<_>= data.split("message:").collect();
+        let data: String = msg[1].to_string();
+        // let mut data_file = fs::OpenOptions::new()
+        // .append(true)
+        // .open("send.txt")
+        // .expect("cannot open file");
+        let _ = fs::write("send.txt", data);
+        // let _ = data_file.write(data.as_bytes());
+    }
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
